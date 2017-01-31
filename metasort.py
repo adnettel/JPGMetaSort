d1034b902f775bce54b489b00553705574accabd
Adam Nettel <netteladam@gmail.com>
2017-01-31 16:35:15 -0600

    Final

--- metasort.py
+++ metasort.py
@@ -1,10 +1,10 @@
 import os, datetime
 from PIL import Image
 
-# Code to identify date created
+# Code to identify date created.
 DATE_CREATED_EXIF_TAG = 306
 
-# Constructs list of (fileString, dateObject)
+# Constructs list of (fileString, dateObject).
 fileDateList = []
 for file in os.listdir("./"):
     if file.lower().endswith("jpg"):
@@ -13,10 +13,10 @@ for file in os.listdir("./"):
         date = datetime.datetime.strptime(info[DATE_CREATED_EXIF_TAG], '%Y:%m:%d %H:%M:%S')
         fileDateList.append((file, date))
 
-# Sorts list of (f, d) based on d
+# Sorts list of (f, d) based on d.
 fileDateList.sort(key=lambda f: f[1])
 
-# Renames files in current folder based on their order in fileDateList
+# Renames files in current folder based on their order in fileDateList.
 # Starts at 000000.jpg
 index = 0
 for fileDate in fileDateList:
